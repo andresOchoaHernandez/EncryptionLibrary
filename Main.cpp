3fa70b7dc818cb9ceb0ec0a76e7f976032b369ff
andres <andres@desktop.haslerrail>
2025-02-26 15:17:19 +0100

    Set up cmake

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,9 @@
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <openssl/conf.h>
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+
 #include <iostream>
 
 int main(int argc, char* argv[])
@@ -6,3 +12,13 @@ int main(int argc, char* argv[])
 
     return 0;
 }
+
+EVP_PKEY *generate_key_pair();
+
+bool encrypt(const unsigned char *plaintext, int plaintext_len, const unsigned char *key, const unsigned char *iv, unsigned char *ciphertext, int &ciphertext_len);
+
+bool decrypt(const unsigned char *ciphertext, int ciphertext_len, const unsigned char *key, const unsigned char *iv, unsigned char *plaintext, int &plaintext_len);
+
+bool sign(EVP_PKEY *private_key, const unsigned char *message, int message_len, unsigned char *signature, unsigned int &signature_len);
+
+bool verify(EVP_PKEY *public_key, const unsigned char *message, int message_len, const unsigned char *signature, unsigned int signature_len);
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # EncryptedCommunication
 This project's goal is to implement a client-server comunication using asymmetric and simmetric encryption with integrity and authenticity checks
+
+# Developement environment
+This project was developed on Ubuntu 20.04.6 LTS
+
+# Dependencies
+openssl : 
+```bash
+sudo apt install libssl-dev
+```
+doxygen : 
+```bash
+sudo apt install doxygen
+```
+graphviz : 
+```bash
+sudo apt install graphviz
+```
