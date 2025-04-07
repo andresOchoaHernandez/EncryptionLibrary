0e028097ccaaa80e4bd54062eaa1d99549bfd6b3
andresOchoaHernandez <ada.wong.gv@gmail.com>
2025-04-07 22:49:28 +0200

    Implemented RSA encryption and decryption

--- src/RSAEncryptorHandler.cpp
+++ src/RSAEncryptorHandler.cpp
@@ -7,12 +7,42 @@
 #include <openssl/evp.h>
 #include <openssl/rsa.h>
 #include <openssl/pem.h>
+#include <openssl/decoder.h>
+
+#include <vector>
 
 #include "RSAEncryptorHandler.hpp"
 
-EVP_PKEY* keypairToEVP_PKEY(RsaKeyPair keyPair)
+EVP_PKEY* stringToEVP_PKEY(const std::string& stringKey,int keyType)
 {
-    return nullptr;
+    const unsigned char* key = (const unsigned char*)stringKey.c_str();
+    size_t keylen = stringKey.size();
+
+    EVP_PKEY* pkey = nullptr;
+
+    OSSL_DECODER_CTX* dctx = OSSL_DECODER_CTX_new_for_pkey(&pkey, "PEM", nullptr, "RSA", keyType, nullptr, nullptr);
+    if (dctx == nullptr) {
+        throw CryptoHandlerException("Failed to create OSSL_DECODER_CTX");
+    }
+
+    int decodeFromDataResult = OSSL_DECODER_from_data(dctx, &key, &keylen);
+    if (decodeFromDataResult <= 0) {
+        throw CryptoHandlerException("Failed to decode PEM key from data");
+    }
+
+    OSSL_DECODER_CTX_free(dctx);
+
+    return pkey;
+}
+
+EVP_PKEY* privateKeyStringToEVP_PKEY(const std::string& privateKey)
+{
+    return stringToEVP_PKEY(privateKey,OSSL_KEYMGMT_SELECT_KEYPAIR);
+}
+
+EVP_PKEY* publicKeyStringToEVP_PKEY(const std::string& publicKey)
+{
+    return stringToEVP_PKEY(publicKey,OSSL_KEYMGMT_SELECT_PUBLIC_KEY);
 }
 
 RsaKeyPair EVP_PKEYToKeypair(EVP_PKEY* keyPair)
@@ -76,10 +106,113 @@ RsaKeyPair RSAEncryptorHandler::generateKeyPair(int bits)
 
 std::string RSAEncryptorHandler::encrypt(const std::string& plaintext,const std::string& key)
 {
-    return "";
+    EVP_PKEY *publicKey = publicKeyStringToEVP_PKEY(key);
+
+    EVP_PKEY_CTX *ctx = EVP_PKEY_CTX_new(publicKey, nullptr);
+    if(ctx == nullptr){
+        throw CryptoHandlerException("Failed to create EVP_PKEY_CTX object");
+    }
+
+    int initializeEncryptionResult = EVP_PKEY_encrypt_init(ctx);
+    if (initializeEncryptionResult <= 0) {
+        EVP_PKEY_CTX_free(ctx);
+        throw CryptoHandlerException("Failed to initialize the encryption object");
+    }
+
+    int setRsaPaddingResult = EVP_PKEY_CTX_set_rsa_padding(ctx, RSA_PKCS1_OAEP_PADDING);
+    if (setRsaPaddingResult <= 0) {
+        EVP_PKEY_CTX_free(ctx);
+        throw CryptoHandlerException("Failed to set the RSA padding");
+    }
+
+    size_t cipertextLength;
+
+    int determineOutputLengthResult = EVP_PKEY_encrypt(
+        ctx,
+        nullptr, 
+        &cipertextLength,
+        reinterpret_cast<unsigned char*>(const_cast<char*>(plaintext.c_str())), 
+        static_cast<int>(plaintext.length())
+    );
+    
+    if (determineOutputLengthResult <= 0) {
+        EVP_PKEY_CTX_free(ctx);
+        throw CryptoHandlerException("Failed to determine the cyphertext length");
+    }
+
+    std::vector<unsigned char> cyphertext(cipertextLength);
+
+    int encryptionResult = EVP_PKEY_encrypt(
+        ctx,
+        cyphertext.data(), 
+        &cipertextLength,
+        reinterpret_cast<unsigned char*>(const_cast<char*>(plaintext.c_str())), 
+        static_cast<int>(plaintext.length())
+    );
+    
+    if (encryptionResult <= 0) {
+        EVP_PKEY_CTX_free(ctx);
+        throw CryptoHandlerException("Failed to encrypt the plaintext");
+    }
+
+    EVP_PKEY_free(publicKey);
+    EVP_PKEY_CTX_free(ctx);
+
+    return std::string(cyphertext.begin(), cyphertext.end());
 }
 
 std::string RSAEncryptorHandler::decrypt(const std::string& cyphertext,const std::string& key)
 {
-    return "";
+    EVP_PKEY *privateKey = privateKeyStringToEVP_PKEY(key);
+
+    EVP_PKEY_CTX *ctx = EVP_PKEY_CTX_new(privateKey, nullptr);
+    if(ctx == nullptr){
+        throw CryptoHandlerException("Failed to create EVP_PKEY_CTX object");
+    }
+
+    int initializeDecryptionResult = EVP_PKEY_decrypt_init(ctx);
+    if (initializeDecryptionResult <= 0) {
+        EVP_PKEY_CTX_free(ctx);
+        throw CryptoHandlerException("Failed to initialize the decryption object");
+    }
+
+    int setRsaPaddingResult = EVP_PKEY_CTX_set_rsa_padding(ctx, RSA_PKCS1_OAEP_PADDING);
+    if (setRsaPaddingResult <= 0) {
+        EVP_PKEY_CTX_free(ctx);
+        throw CryptoHandlerException("Failed to set the RSA padding");
+    }
+
+    size_t plainTextLength;
+
+    int determineOutputLengthResult = EVP_PKEY_decrypt(
+        ctx,
+        nullptr, 
+        &plainTextLength,
+        reinterpret_cast<unsigned char*>(const_cast<char*>(cyphertext.c_str())), 
+        static_cast<int>(cyphertext.length())
+    );
+    
+    if (determineOutputLengthResult <= 0) {
+        EVP_PKEY_CTX_free(ctx);
+        throw CryptoHandlerException("Failed to determine the plaintext length");
+    }
+    std::vector<unsigned char> plaintext(plainTextLength);
+
+    int decryptionResult = EVP_PKEY_decrypt(
+        ctx,
+        plaintext.data(), 
+        &plainTextLength,
+        reinterpret_cast<unsigned char*>(const_cast<char*>(cyphertext.c_str())), 
+        static_cast<int>(cyphertext.length())
+    );
+    
+    if (decryptionResult <= 0) {
+        EVP_PKEY_CTX_free(ctx);
+        throw CryptoHandlerException("Failed to decrypt the cyphertext");
+    }
+
+    EVP_PKEY_free(privateKey);
+    EVP_PKEY_CTX_free(ctx);
+
+    return std::string(plaintext.begin(), plaintext.end());
 }
--- test/Test_RSAEncryptorHandler.cpp
+++ test/Test_RSAEncryptorHandler.cpp
@@ -2,14 +2,26 @@
 
 #include <iostream>
 
-int main (int argc, char* argv[])
+bool test_helloWorld()
 {
     RSAEncryptorHandler test;
         
     RsaKeyPair res =  test.generateKeyPair(2048);
 
-    std::cout << res.publicKey << std::endl;
-    std::cout << res.privateKey << std::endl;
+    const std::string message = "Hello world!";
+
+    const std::string cyphertext = test.encrypt(message,res.publicKey);
+    
+    /* RSA at 2048 bits supports messages of maximum 256 bytes */
+    const std::string decryptedMessage = test.decrypt(cyphertext,res.privateKey).substr(0,message.size());
+
+    return message == decryptedMessage;
+}
+
+int main (int argc, char* argv[])
+{
+    if(!test_helloWorld())
+        return -1;
 
     return 0;
 }
